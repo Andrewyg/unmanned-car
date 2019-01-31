a4ff39578f926111d668cd567f6bdf049dda80a6
Andrew <jiganyg@gmail.com>
2019-01-31 14:23:26 +0800

    Update revision.sh

--- revision.sh
+++ revision.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 cd /var/www/
+echo "{\"now_Version_Git_SHA_Num\":" > /usr/share/nginx/html/ver.json
+echo $1 >> /usr/share/nginx/html/ver.json
+echo "}" >> /usr/share/nginx/ver.json
 pm2 stop all
 pm2 delete all
 rm -rf unmanned-car
