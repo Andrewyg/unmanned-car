f1b504f91e3ed7de1963cbf701c6e80c963d4394
Andrew <jiganyg@gmail.com>
2019-01-31 14:47:38 +0800

    update typing error on revision script

--- revision.sh
+++ revision.sh
@@ -2,7 +2,7 @@
 cd /var/www/
 echo "{\"now_Version_Git_SHA_Num\":" > /usr/share/nginx/html/ver.json
 echo $1 >> /usr/share/nginx/html/ver.json
-echo "}" >> /usr/share/nginx/ver.json
+echo "}" >> /usr/share/nginx/html/ver.json
 pm2 stop all
 pm2 delete all
 rm -rf unmanned-car
