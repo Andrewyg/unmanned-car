a3652a1e7bc6f559aca624a19b0c42ecf28f8e7b
Andrew <jiganyg@gmail.com>
2019-02-14 22:47:33 +0800

    v6-test-4

--- simulator.js
+++ simulator.js
@@ -66,7 +66,7 @@
             if (input && output) {
                 cb({ "normalInsTakenTime": normalIns(input, leftTurnTime, straightGoTime, oneCarTime, insLightTime), "computerControledInsTakenTime": ccIns(output) });
             } else {
-                request.get("https://uc.ccsource.org/operate?left=" + leftTurnTime + "&straight=" + straightGoTime + "&right=" + rightTurnTime + "&car=" + oneCarTime, (err, res, body) => {
+                request.get("https://uc.ccsource.org/api/operate?left=" + leftTurnTime + "&straight=" + straightGoTime + "&right=" + rightTurnTime + "&car=" + oneCarTime, (err, res, body) => {
                     var data = JSON.parse(body);
                     var input = data.input,
                         output = data.output;
