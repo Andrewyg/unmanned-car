81400a1f45cfda7fbb2da754f034c3eb6b878635
Andrew <jiganyg@gmail.com>
2019-04-10 21:39:55 +0800

    Update compiler.js

--- compiler.js
+++ compiler.js
@@ -175,7 +175,6 @@
 
                     simulator.run(leftTurnTime, straightGoTime, rightTurnTime, oneCarTime, insLightTimeHS, insLightTimeHL, insLightTimeVS, insLightTimeVL, (rtd992) => {
                         db.result.save(CIns, rtd992._id, movingIns, (rtd991) => {
-                            returnData.output = rtd991;
                             db.compare.setResult(rtd992._id, rtd991._id, (rtd993) => {
                                 db.scene.archive(CIns, copyData, (rtd999) => {
                                     cb(rtd993._id, rtd999._id)
