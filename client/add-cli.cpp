0858bba96e5a65a84ee50870ca54d9483a816aa0
Andrew <jiganyg@gmail.com>
2019-04-11 18:48:53 +0800

    update add-cli and exclude .out file from git

--- client/add-cli.cpp
+++ client/add-cli.cpp
@@ -6,56 +6,57 @@
 
 using namespace std;
 
-vector<string> split(const string& str, const string& delim)
+vector<string> split(const string &str, const string &delim)
 {
-    vector<string> tokens;
-    size_t prev = 0, pos = 0;
-    do
-    {
-        pos = str.find(delim, prev);
-        if (pos == string::npos) pos = str.length();
-        string token = str.substr(prev, pos-prev);
-        if (!token.empty()) tokens.push_back(token);
-        prev = pos + delim.length();
-    }
-    while (pos < str.length() && prev < str.length());
-    return tokens;
+  vector<string> tokens;
+  size_t prev = 0, pos = 0;
+  do
+  {
+    pos = str.find(delim, prev);
+    if (pos == string::npos)
+      pos = str.length();
+    string token = str.substr(prev, pos - prev);
+    if (!token.empty())
+      tokens.push_back(token);
+    prev = pos + delim.length();
+  } while (pos < str.length() && prev < str.length());
+  return tokens;
 }
 
-int main(int argc, char *argv[]) {
-  if(strcmp(argv[0], "cli") == 0) {
+int main(int argc, char *argv[])
+{
+  if (strcmp(argv[0], "cli") == 0)
+  {
     string input;
-    while(getline(cin,input)) {
+    while (getline(cin, input))
+    {
       vector<string> inputArr = split(input, " ");
       string cmd("");
-      cmd+="curl --data \"place=";
-      cmd+=inputArr[0];
-      cmd+="&direction=";
-      cmd+=inputArr[1];
-      cmd+="\" https://ccins.andrew.at.tw/add";
+      cmd += "curl --data \"place=";
+      cmd += inputArr[0];
+      cmd += "&direction=";
+      cmd += inputArr[1];
+      cmd += "\" https://ccins.andrew.at.tw/add";
       system(cmd.c_str());
     }
-  } else {
+  }
+  else
+  {
     ifstream input(argv[1]);
     string line;
-    while (getline(input, line)) {
-      vector<string> inputArr = split(line,",");
+    while (getline(input, line))
+    {
+      vector<string> inputArr = split(line, ",");
       string cmd("");
-      cmd+="curl -d \"place=";
+      cmd += "curl -d \"position=";
+      cmd += inputArr[0];
+      cmd += "&direction=";
+      cmd += inputArr[1];
+      cmd += "\" -X POST https://ccins.andrew.at.tw/add";
       cout << cmd << endl;
-      cmd+=inputArr[0];
-      cout << cmd << endl;
-      cmd+="&direction=";
-      cout << cmd << endl;
-      cmd+=inputArr[1];
-      cout << cmd << endl;
-      cmd+="hahaha";
-      cout << cmd << endl;
-      cmd+="\" -X POST https://ccins.andrew.at.tw/add";
-      cout << cmd << endl;
-      // system(cmd.c_str());
+      system(cmd.c_str());
     }
   }
-  // system("firefox http://localhost/operate");
+  // system("python2.7");
   return 0;
 }
