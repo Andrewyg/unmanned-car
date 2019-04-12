90b154347477dd761c37918b86c608119cc69e16
Andrew@TW <jiganyg@gmail.com>
2019-04-12 14:11:43 +0800

    fix flow issue

--- compiler.js
+++ compiler.js
@@ -223,17 +223,16 @@
                             ],
                             delay: rtd99
                         });
-                    })
-
-                    simulator.run(leftTurnTime, straightGoTime, rightTurnTime, oneCarTime, insLightTimeHS, insLightTimeHL, insLightTimeVS, insLightTimeVL, (rtd992) => {
-                        db.result.save(CIns, rtd992._id, movingIns, (rtd991) => {
-                            db.compare.setResult(rtd992._id, rtd991._id, (rtd993) => {
-                                db.scene.archive(CIns, copyData, (rtd999) => {
-                                    cb(rtd991._id, rtd992._id, rtd999._id)
+                        simulator.run(leftTurnTime, straightGoTime, rightTurnTime, oneCarTime, insLightTimeHS, insLightTimeHL, insLightTimeVS, insLightTimeVL, (rtd992) => {
+                            db.result.save(CIns, rtd992._id, movingIns, (rtd991) => {
+                                db.compare.setResult(rtd992._id, rtd991._id, (rtd993) => {
+                                    db.scene.archive(CIns, copyData, (rtd999) => {
+                                        cb(rtd991._id, rtd992._id, rtd999._id)
+                                    })
                                 })
                             })
-                        })
-                    }, cinsP, movingIns);
+                        }, cinsP, movingIns);
+                    })
                 })
             })
         }
